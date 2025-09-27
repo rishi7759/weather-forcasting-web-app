f5f57e96e645ca34a10d316f099a57b0cc9d2048
rishi <rishikeshsingh526@gmail.com>
2025-09-27 13:28:30 +0530

    minor changes

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const apikey = "e5d2bf882223abcf32bec627baf177b3";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon"); 
+
+async function checkweather(city) {
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block"; 
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        const data = await response.json(); 
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+       
+        if (data.weather[0].main.toLowerCase() === "clouds") {
+            weatherIcon.src = "cloud.jpg";
+        } else if (data.weather[0].main.toLowerCase() === "clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main.toLowerCase() === "rain") {
+            weatherIcon.src = "rainyy.jpg";
+        } else if (data.weather[0].main.toLowerCase() === "drizzle") {
+            weatherIcon.src = "drizzle.jpg";
+        } else if (data.weather[0].main.toLowerCase() === "mist") {
+            weatherIcon.src = "mist.jpg";
+        } else {
+            weatherIcon.src = "images/default.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkweather(searchBox.value); 
+});
+
+
+
+
